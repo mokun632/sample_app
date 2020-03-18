9036939093906697269d73d31177248ecae5f271
Motohitro Yamagishi <motohiro993@gmail.com>
2020-03-18 23:19:47 +0900

    user model

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_08_062343) do
+ActiveRecord::Schema.define(version: 2020_03_16_095013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
-
-  create_table "microposts", force: :cascade do |t|
-    t.text "content"
-    t.integer "user_id"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
