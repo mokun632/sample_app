397b269a130fa3ee5a97b2110efb702d3eb196cf
Motohitro Yamagishi <motohiro993@gmail.com>
2020-04-16 01:49:12 +0900

    rename4

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -4,8 +4,8 @@ if Rails.env.production?
       # Amazon S3用の設定
       :provider              => 'AWS',
       :region                => ENV['ap-northeast-1'],
-      :aws_access_key_id     => ENV['ACCESS_KEY_ID'],
-      :aws_secret_access_key => ENV['SECRET_ACCESS_KEY']
+      :aws_access_key_id     => ENV['AKIA4BHA225Y7OARLAXD'],
+      :aws_secret_access_key => ENV['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']
     }
     config.fog_directory     =  ENV['railstutorial231469']
   end
