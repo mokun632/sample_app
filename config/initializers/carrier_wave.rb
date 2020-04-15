feaf643fa34da6d33c825633634b71696bf44a04
Motohitro Yamagishi <motohiro993@gmail.com>
2020-04-16 08:51:00 +0900

    carrierwave

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -3,10 +3,11 @@ if Rails.env.production?
     config.fog_credentials = {
       # Amazon S3用の設定
       :provider              => 'AWS',
-      :region                => ENV['ap-northeast-1'],
-      :aws_access_key_id     => ENV['AKIA4BHA225Y7OARLAXD'],
-      :aws_secret_access_key => ENV['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']
+      :region                => ENV['S3_REGION'],     # 例: 'ap-northeast-1'
+      :aws_access_key_id     => ENV['S3_ACCESS_KEY'],
+      :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory     =  ENV['railstutorial231469']
+    config.fog_directory     =  ENV['S3_BUCKET']
+  end
   end
 end
